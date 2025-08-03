f0b2bc95b3e39dd5070aa4f558ab0f7c81714264
AVS <cristoffstan@gmail.com>
2025-08-03 11:27:59 +0330

    Implement API client

---
+++ client/client.go
@@ -0,0 +1,62 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"errors"
+
+	"github.com/Metrika-Inc/blckmock"
+	"smartblox-ingestor/types"
+
+)
+
+ErrBlockNotFound := errors.New("block not found")
+
+
+// Communicating with the SmartBlox node API
+type APIClient interface{
+	GetStatus(ctx context.Context) (*types.Status, error)
+	GetBlock(ctx context.Context, round string) (*types.Block, error)
+}
+
+type smartBloxClient struct{}
+
+// Create a new client for the API
+func NewSmartBloxClient() APIClient {
+	return &smartBloxClient{}
+}
+
+// Fetches the latest round (status) from the API
+func (s *smartBloxClient) GetStatus(ctx context.Context) (*types.Status, error) {
+	res, err := blckmock.GetStatus()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get status from /api/status: %w", err)
+	}
+
+	status:= types.Status
+	if err := json.Unmarshal(res, &status); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal status response: %w", err)
+	}
+
+	return &status, nil
+}
+
+// Fetches a block from the API
+func (s *smartBloxClient) GetBlock(ctx context.Context, round uint64) (*types.Block, error) {
+	res, err := blckmock.GetBlock(round)
+	if err != nil {
+		if err.Error() == "not found" {
+			return nil, ErrBlockNotFound
+		}
+		return nil, fmt.Errorf("failed to get block from /api/blocks/%d: %w", round, err)
+	}
+
+	block:= types.Block
+	if err := json.Unmarshal(res, &block); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal to unmarshal block response: %w", err)
+	}
+
+	return &block, nil	
+
+}
